7424879c2956e221fa758828aafb1aa7f0539cc8
QABuilder <qabuilder@nvidia.com>
2018-03-03 22:29:08 -0800

    Can I Win: bottom up not easy and not accepted
    For a certain pattern of remain integers, only one left in pile is
    possible. So to compute all combination of remain integers and left
    target isn't necessary and is time consuming.
    
    Maybe there is a way by which I can find all possible last steps and
    from there construct path back to target. But I haven't found a easy way
    to find out the all possible last steps. For example, when
    maxChoosableInteger=10, desiredTotal=20, one of the last steps can be
    remain integers={1,2,3,4,6,7,8,9}, target=5.
    
    Will try dfs.

--- CanIWin/main.cpp
+++ CanIWin/main.cpp
@@ -14,14 +14,14 @@ public:
         for (long sign = 0; sign <= max_sign; sign++) {
             m[0][sign] = -1;
         }
-        m[1] = vector<int>(max_sign + 1, 1);
+        m[1] = vector<int>(max_sign + 1, maxChoosableInteger+1);
         m[1][0] = 0;
 
         for (int i = 2; i <= desiredTotal; i++) {
             construct_next(m, i, maxChoosableInteger, max_sign);
         }
 
-        return m[desiredTotal%maxChoosableInteger][(1<<maxChoosableInteger)-1] == 1;
+        return m[desiredTotal%maxChoosableInteger][(1<<maxChoosableInteger)-1] > 0;
     }
 
     // given a target and left choosable integers
@@ -30,18 +30,20 @@ public:
     // otherwise, marked 0
     void construct_next(vector<vector<int>> &m, int target, int maxC, int max_sign) {
         int slot = target % maxC;
-        vector<int> newrule(max_sign + 1, 0);
+        vector<int> newrule(max_sign + 1, maxC+1);
 
-        for (long sign = 1; sign <= max_sign; sign++) {
-            if ((sign >> (target -1)) >= 1) {
-                newrule[sign] = 1;
-                continue;
+        for (unsigned long sign = 1; sign <= max_sign; sign++) {
+            if ((sign >= (1UL << (target-1)))) {
+                newrule[sign] = maxC+1;
+                break;
             }
             bool must_lose = true;
-            for (int takeoff = 1; takeoff <= min(maxC, target); takeoff++) {
-                if (sign & (1<<(takeoff-1))==0)
+            for (int takeoff = 1; takeoff <= min(maxC, target-1); takeoff++) {
+                if ((1UL << (takeoff-1)) > sign)
+                    break;
+                if ((sign & (1UL<<(takeoff-1))) == 0 )
                     continue;
-                int nsign = sign ^ (1<<(takeoff-1));
+                int nsign = sign ^ (1UL<<(takeoff-1));
                 int s = (target - takeoff + maxC) % maxC;
                 if (m[s].size() == 0) {
                     continue;
@@ -52,15 +54,9 @@ public:
                     break;
                 }
                 if (m[s][nsign] == -1) {
-                    if ((target == 40) && (sign == max_sign)) {
-                        cout << target << ": first take off " << takeoff << endl;
-                    }
-                    newrule[sign] = 1;
+                    newrule[sign] = takeoff;
                     must_lose = false;
                     break;
-                }
-                if (m[s][nsign] != 1) {
-                    must_lose = false;
                 }
             }
             if (must_lose) {
@@ -73,7 +69,7 @@ public:
 
 int main() {
     Solution slt;
-    bool ret = slt.canIWin(10, 40);
+    bool ret = slt.canIWin(20, 209);
     if (ret)
         cout << "win" << endl;
     else
--- CanIWin_validation/main.cpp
+++ CanIWin_validation/main.cpp
@@ -8,14 +8,14 @@ public:
     vector<vector<int>> canIWin(int maxChoosableInteger, int desiredTotal) {
         //if (desiredTotal <= maxChoosableInteger) return true;
 
-        vector<vector<int>> m(maxChoosableInteger, vector<int>(0));
+        vector<vector<int>> m(desiredTotal + 1, vector<int>(0));
 
         long max_sign = (1UL << maxChoosableInteger) - 1;
         m[0] = vector<int>(max_sign + 1, 0);
         for (long sign = 0; sign <= max_sign; sign++) {
             m[0][sign] = -1;
         }
-        m[1] = vector<int>(max_sign + 1, 1);
+        m[1] = vector<int>(max_sign + 1, maxChoosableInteger+1);
         m[1][0] = 0;
 
         for (int i = 2; i <= desiredTotal; i++) {
@@ -30,20 +30,22 @@ public:
     // when with any integer, other person must win, then integer+sign marked -1
     // otherwise, marked 0
     void construct_next(vector<vector<int>> &m, int target, int maxC, int max_sign) {
-        int slot = target % maxC;
+        int slot = target;
         vector<int> newrule(max_sign + 1, 0);
 
-        for (long sign = 1; sign <= max_sign; sign++) {
-            if ((sign >> (target -1)) >= 1) {
-                newrule[sign] = 1;
+        for (unsigned long sign = 1; sign <= max_sign; sign++) {
+            if ((sign >= (1UL << (target-1)))) {
+                newrule[sign] = maxC + 1;
                 continue;
             }
             bool must_lose = true;
-            for (int takeoff = 1; takeoff <= min(maxC, target); takeoff++) {
-                if (sign & (1<<(takeoff-1))==0)
+            for (int takeoff = 1; takeoff <= min(maxC, target-1); takeoff++) {
+                if ((1UL << (takeoff-1)) > sign)
+                    break;
+                if ((sign & (1UL<<(takeoff-1))) == 0 )
                     continue;
-                int nsign = sign ^ (1<<(takeoff-1));
-                int s = (target - takeoff + maxC) % maxC;
+                int nsign = sign ^ (1UL<<(takeoff-1));
+                int s = target - takeoff;
                 if (m[s].size() == 0) {
                     continue;
                 }
@@ -57,9 +59,6 @@ public:
                     must_lose = false;
                     break;
                 }
-                if (m[s][nsign] != 1) {
-                    must_lose = false;
-                }
             }
             if (must_lose) {
                 newrule[sign] = -1;
@@ -71,17 +70,22 @@ public:
 
 int main() {
     Solution slt;
-    auto m = slt.canIWin(10, 32);
-    long sign = 0x3BF;
-
-    for (int i = 10; i > 0; i--) {
-        int newvalue = 33 - i;
-        long newsign = sign ^ (1UL << (i-1));
-        cout << newvalue << "+" << newsign << ": first take off " << m[newvalue%10][newsign] << endl;
+    auto m = slt.canIWin(10, 40);
+    if (m[40][1023] > 0) {
+        cout << "win: " << m[40][1023] << endl;
+    } else {
+        cout << "lose: " << m[40][1023] << endl;
     }
 
-    char a;
-    cin >> a;
+    int a;
+    do {
+        cin >> a;
+        cout << "printing rules for " << a << endl;
+        for (int i = 0; i < 1024; i++) {
+            cout << std::hex << i <<std::dec << ":" << m[a][i] << "\t";
+        }
+        cout << endl;
+    } while (a != 0);
 
     return 0;
 }
