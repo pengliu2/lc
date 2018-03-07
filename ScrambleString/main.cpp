c8d68e702ef48aac01ecf0cf7da5406d777e204b
QABuilder <qabuilder@nvidia.com>
2018-03-06 22:49:14 -0800

    Scramble String: accepted and time okay

--- ScrambleString/main.cpp
+++ ScrambleString/main.cpp
@@ -5,22 +5,19 @@ class Solution {
 public:
     bool isScramble(string s1, string s2) {
         if (s1.size() != s2.size()) return false;
-        if (s1.size() == 0) return true;
+        if (s1.empty()) return true;
 
         int size = s1.size();
         auto cache = unordered_map<string, unordered_map<string, bool>>();
 
-        return dfs(cache, s1, 0, size - 1, s2, 0, size -1, size);
+        return dfs(cache, s1, 0, size - 1, s2, 0, size -1);
     }
 
-    bool dfs(unordered_map<string, unordered_map<string, bool>> &cache, string &s1, int b1, int e1, string &s2, int b2, int e2, int size) {
-        if (b1 == e1) {
-            if (s1[b1] == s2[b2])
-                return true;
-            else
-                return false;
-        }
-        if (s1.compare(b1, e1-b1+1, s2, b2, e2-b2+1) == 0) return true;
+    bool dfs(unordered_map<string, unordered_map<string, bool>> &cache, string &s1, int b1, int e1, string &s2, int b2, int e2) {
+        if (s1.compare(b1, e1-b1+1, s2, b2, e2-b2+1) == 0)
+            return true;
+        else if (s1.size() == 1)
+            return false;
 
         if (b1 + 1 == e1) {
             if ((s1[b1] == s2[e2]) && (s1[e1] == s2[b2]))
@@ -38,24 +35,38 @@ public:
             }
         }
 
+        int total = 0;
+        for (int i = 0; i <= len; i++) {
+            total += s1.c_str()[b1+i] - s2.c_str()[b2+i];
+        }
+        if (total != 0) {
+            cache[sub1][sub2] = false;
+            cache[sub2][sub1] = false;
+            return false;
+        }
+
         for (int i = 0; i < e1-b1; i++) {
-            if (dfs(cache, s1, b1, b1 + i, s2, b2, b2 + i, size)
-                && dfs(cache, s1, b1 + i + 1, e1, s2, b2 + i + 1, e2, size)) {
+            if (dfs(cache, s1, b1, b1 + i, s2, b2, b2 + i)
+                && dfs(cache, s1, b1 + i + 1, e1, s2, b2 + i + 1, e2)) {
                 cache[sub1][sub2] = true;
+                cache[sub2][sub1] = true;
                 return true;
             }
-            if (dfs(cache, s1, b1, b1+i, s2, b2 + len - i, e2, size)
-                    && dfs(cache, s1, b1+len-i, e1, s2, b2+i, e2, size)) {
+            if (dfs(cache, s1, b1, b1+i, s2, e2 - i, e2)
+                    && dfs(cache, s1, b1+i+1, e1, s2, b2, e2-i-1)) {
                 cache[sub1][sub2] = true;
+                cache[sub2][sub1] = true;
                 return true;
             }
         }
         cache[sub1][sub2] = false;
+        cache[sub2][sub1] = false;
         return false;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution slt;
+    slt.isScramble("great", "tgera");
     return 0;
 }
