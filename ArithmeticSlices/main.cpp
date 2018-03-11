4a105bee642ed89c2f2146dfe27dcdbc4a5ac0d9
Peng Liu <chineseliupeng@gmail.com>
2018-03-10 16:28:36 -0800

    Arithmetic Slices: still slow

--- ArithmeticSlices/main.cpp
+++ ArithmeticSlices/main.cpp
@@ -4,28 +4,18 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-        auto size = A.size();
-        if (size < 3) return 0;
-
-        int total = 0;
-        auto cache = vector<bool>(size, false);
-        for (int i = 0; i < size - 2; i++) {
-            if (A[i+2] - A[i+1] == A[i+1] - A[i]) {
-                cache[i] = true;
-                total += 1;
+        if (A.size() < 3) return 0;
+        int curr = 0;
+        int sum = 0;
+        for (int i=2; i<A.size(); i++){
+            if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+                curr += 1;
+                sum += curr;
+            } else {
+                curr = 0;
             }
         }
-
-        for (int i = 4; i <= size; i++) {
-            for (int j = 0; j <= size - i; j++) {
-                if (cache[j] && cache[j+1]) {
-                    total += 1;
-                } else {
-                    cache[j] = false;
-                }
-            }
-        }
-        return total;
+        return sum;
     }
 };
 
