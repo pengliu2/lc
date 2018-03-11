40c18c9fac2a38715a50c4f8e8a3929337d566ca
Peng Liu <chineseliupeng@gmail.com>
2018-03-11 01:34:50 -0800

    Unique Substring in Wraparound String: accepted

--- UniqueSubstringsInWraparoundString/main.cpp
+++ UniqueSubstringsInWraparoundString/main.cpp
@@ -7,44 +7,31 @@ public:
         int size = p.length();
         if (size == 0) return 0;
 
+        vector<int> maxlen = vector<int>(26, 0);
+        maxlen[p[0]-'a'] = 1;
+        int start = 0;
+        int len = 1;
+        for (int i = 1; i < size; i++) {
+            if (p[i] % 26 == (p[i-1] + 1) % 26) {
+                len++;
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    maxlen[p[start + j] - 'a'] = max(maxlen[p[start + j] - 'a'], len);
+                    len--;
+                    if (len == 0) break;
+                }
+                len = 1;
+                start = i;
+            }
+        }
+        for (int j = 0; j < 26; j++) {
+            maxlen[p[start + j] - 'a'] = max(maxlen[p[start + j] - 'a'], len);
+            len--;
+            if (len == 0) break;
+        }
         int total = 0;
-        unsigned int scache = 0;
-        auto cache = set<int>();
-        for (int i = 0;  i < size; i++) {
-            if (((scache >> (p[i] - 'a')) & 0x01UL) != 0) continue;
-            scache = scache | (0x01UL << (p[i] - 'a'));
-            total++;
-        }
-        scache = 0;
-        for (int j = 0; j < size -1; j++) {
-            if (((scache >> (p[j] - 'a')) & 0x01UL) != 0) {
-                cache.insert(j);
-                continue;
-            }
-            if ((p[j] - 'a' + 1)%26 == (p[j+1]-'a')%26) {
-                total++;
-                scache = scache | (0x01UL << (p[j] - 'a'));
-                cache.insert(j);
-            }
-        }
-
-        for (int i = 3; i <= size; i++) {
-            scache = 0;
-            if (cache.empty()) break;
-            for (set<int>::iterator it = cache.begin(); it != cache.end();) {
-                if (((scache >> (p[*it] - 'a')) & 0x01UL) != 0) {
-                    it++;
-                    continue;
-                }
-                if ((p[*it+i-2] - 'a' + 1)%26 == (p[*it+i-1]-'a')%26) {
-                    total++;
-                    scache = scache | (0x01UL << (p[*it] - 'a'));
-                    it++;
-                } else {
-                    it=cache.erase(it);
-                }
-            }
-
+        for (int i = 0; i < 26; i++) {
+            total += maxlen[i];
         }
         return total;
     }
