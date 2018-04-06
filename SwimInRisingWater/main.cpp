e8a8d3163579b386f76bb93b2d220b0494f0930a
Peng Liu <chineseliupeng@gmail.com>
2018-04-06 01:04:46 -0700

    Swim in Rising Water: accepter and fast

--- SwimInRisingWater/main.cpp
+++ SwimInRisingWater/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 /*
  * reminder:
  * 1. don't forget inner loop is cols, outer is rows
+ * 2. because numbers in matrix are 0..cols*rows-1, we can just memorize their positions and only check 1 number per loop
+ * 3. we can also not update numbers in grid. At time t, any numbers smaller than t can be connected.
  */
 
 class Solution {
@@ -14,30 +16,30 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
         ids = vector<vector<int>>(rows*cols, vector<int>{0, 0});
+        vector<int> pos(rows*cols);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 ids[i * cols + j][0] = i * cols + j;
                 ids[i * cols + j][1] = 1;
+                pos[grid[i][j]] = i*cols + j;
             }
         }
         int ret = 0;
         for (ret = 0; ret < rows * cols; ret++) {
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < cols; k++) {
-                    if (j > 0 && grid[j][k] == grid[j-1][k]) {
-                        ids[j*cols + k][0] = unite(findparent(j*cols+k), findparent((j-1)*cols + k));
-                    }
-                    if (k > 0 && grid[j][k] == grid[j][k-1]) {
-                        ids[j*cols + k][0] = unite(findparent(j*cols+k), findparent(j*cols + k -1));
-                    }
-                }
+            int r = pos[ret] / cols;
+            int c = pos[ret] % cols;
+            if (r > 0 && grid[r - 1][c] < grid[r][c])
+                ids[r * cols + c][0] = unite(findparent((r - 1) * cols + c), findparent(r * cols + c));
+            if (r < rows - 1 && grid[r + 1][c] < grid[r][c]) {
+                ids[r * cols + c][0] = unite(findparent((r + 1) * cols + c), findparent(r * cols + c));
             }
-            if (findparent(rows*cols-1) == findparent(0)) break;
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < cols; k++) {
-                    grid[j][k] = max(grid[j][k], ret+1);
-                }
-            }
+            if (c > 0 && grid[r][c - 1] < grid[r][c])
+                ids[r * cols + c][0] = unite(findparent(r * cols + c - 1), findparent(r * cols + c));
+            if (c < cols - 1 && grid[r][c + 1] < grid[r][c])
+                ids[r * cols + c][0] = unite(findparent(r * cols + c + 1), findparent(r * cols + c));
+
+            if (findparent(rows * cols - 1) == findparent(0)) break;
+
         }
         return ret;
     }
