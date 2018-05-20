f593b4d906d783ec3b6922a29002ae46770e5b0b
Peng Liu <chineseliupeng@gmail.com>
2018-05-20 00:00:02 -0700

    TargetSum: DFS solution accepted

--- TargetSum/main.cpp
+++ TargetSum/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include "../myheader.h"
 
-class Solution {
+/*
+ * reminder:
+ * 1. helper needs to return a value, which is more easily get children's returns
+ * 2. the cache must be able to cover all possible -sum(nums) to sum(nums)
+ * 3. reversed DFS like solutionII: better to check target reachability or we have to keep a larger cache
+ */
+
+class SolutionII {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        sum = accumulate(nums.begin(), nums.end(), 0);
+        cache = vector<vector<int>>(nums.size(), vector<int>(sum*2+2, -1));
+        return helper(S, nums, 0);
+    }
+
+    int helper(int S, vector<int>& nums, int index) {
+        if (S > sum || S + sum < 0) return 0;
+        if (index == nums.size()) {
+            if (S == 0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        if (cache[index][S + sum + 1] != -1) {
+            //cout << "c:" << index << "," << S << "at " << S+sum+1 << ":" << cache[index][S + sum + 1] << endl;
+            return cache[index][S + sum + 1];
+        }
+
+        int minus = helper(S - nums[index], nums, index+1);
+        int plus = helper(S + nums[index], nums, index+1);
+        cache[index][S + sum + 1] = minus + plus;
+        return minus + plus;
+    }
+private:
+    vector<vector<int>> cache;
+    int sum;
+};
+
+class SolutionI {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
         vector<unordered_map<int, int>> cache = vector<unordered_map<int, int>>(nums.size() + 1);
